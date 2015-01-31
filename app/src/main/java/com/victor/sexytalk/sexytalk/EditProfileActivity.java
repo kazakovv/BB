3b227874212f694fe04bd9fedcaf15ea7dbf0429
kazakovv <kazakov.vcictor@gmail.com>
2015-01-31 15:22:12 +0000

    napravih go!

--- app/src/main/java/com/victor/sexytalk/sexytalk/EditProfileActivity.java
+++ app/src/main/java/com/victor/sexytalk/sexytalk/EditProfileActivity.java
@@ -1,18 +1,38 @@
 package com.victor.sexytalk.sexytalk;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class EditProfileActivity extends ActionBarActivity {
-
+    protected Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
+        //setContentView(R.layout.activity_edit_profile);
+
+        //fragment
+        MyFragment fragment = new MyFragment();
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
+
+
     }
+
 
 
     @Override
@@ -31,9 +51,36 @@ public class EditProfileActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this,"Clicked",Toast.LENGTH_LONG).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static class MyFragment extends ListFragment {
+        protected ListView editProfileOptionsList;
+        protected Toolbar toolbar;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View inflatedView = inflater.inflate(R.layout.activity_edit_profile,container,false);
+            toolbar = (Toolbar) inflatedView.findViewById(R.id.toolbar);
+            toolbar.setNavigationIcon(R.drawable.ic_action_back);
+            ((EditProfileActivity)getActivity()).setSupportActionBar(toolbar);
+
+            return inflatedView;
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            editProfileOptionsList = getListView();
+            ArrayAdapter<CharSequence> arrayAdapter =
+                    ArrayAdapter.createFromResource(getActivity(), R.array.edit_profile_options, android.R.layout.simple_spinner_dropdown_item);
+
+            editProfileOptionsList.setAdapter(arrayAdapter);
+        }
+    }
+
+
 }
