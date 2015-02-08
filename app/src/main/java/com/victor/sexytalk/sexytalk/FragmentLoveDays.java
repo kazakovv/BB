511ab6674eee9aaab196b57b80b23929dea6a6a1
kazakovv <kazakov.vcictor@gmail.com>
2015-02-08 16:28:33 +0000

    na krastopat

--- app/src/main/java/com/victor/sexytalk/sexytalk/FragmentLoveDays.java
+++ app/src/main/java/com/victor/sexytalk/sexytalk/FragmentLoveDays.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
+import android.support.v4.app.ListFragment;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
