937c9f0d14fe1d9bd89573062d17acd0aaea0ce9
kazakovv <kazakov.vcictor@gmail.com>
2015-02-23 22:14:33 +0000

    2 buttons added

--- app/src/main/java/com/victor/sexytalk/sexytalk/Adaptors/AdapterSearchPartners.java
+++ app/src/main/java/com/victor/sexytalk/sexytalk/Adaptors/AdapterSearchPartners.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -46,7 +47,7 @@ public class AdapterSearchPartners extends ArrayAdapter<BackendlessUser> {
             holder = new ViewHolder();
             holder.nameLabel = (TextView) convertView.findViewById(R.id.partnerUsername);
             holder.iconImageView = (ImageView) convertView.findViewById(R.id.thumbnail_partner);
-            holder.buttonAddPartner = (Button) convertView.findViewById(R.id.addPartnerButton);
+            holder.buttonAddPartner = (ImageButton) convertView.findViewById(R.id.addPartnerButton);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
@@ -77,7 +78,7 @@ public class AdapterSearchPartners extends ArrayAdapter<BackendlessUser> {
     private static class ViewHolder {
         ImageView iconImageView;
         TextView nameLabel;
-        Button buttonAddPartner;
+        ImageButton buttonAddPartner;
     }
 
 
